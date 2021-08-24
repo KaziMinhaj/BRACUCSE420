bf00393730d1ea329cda5544dff15e00ecbc19e8
Kazi Minhajul Haider <68158190+KaziMinhaj@users.noreply.github.com>
2021-08-24 22:51:16 +0600

    Add files via upload

---
+++ LAB01/src/lab01/CSE420_LAB1.java
@@ -0,0 +1,12 @@
+package lab01;
+
+public class CSE420_LAB1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LexicalAnalyzer la=new LexicalAnalyzer();
+		la.anlyze();
+
+	}
+
+}
